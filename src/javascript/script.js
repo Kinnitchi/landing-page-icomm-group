243d6eec0bbdb5df786b2ff6d6dddd368eb3cd44
Kinnitchi <igorivieira97@gmail.com>
2022-04-07 15:16:50 -0300

    trabalhando com a responsividade do layout

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,6 +1,6 @@
 $('nav a[href^="#"]').on('click', function (e) {
   e.preventDefault();
-  var id = $(this).attr('href'),
+  let id = $(this).attr('href'),
     targetOffset = $(id).offset().top;
 
   $('html, body').animate({
